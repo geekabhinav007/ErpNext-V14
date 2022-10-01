6656ceea5e256a81cbc893098a48cc84982c6871
Kumar Abhinav <abhinav26january@gmail.com>
2022-10-01 15:34:34 +0530

    Added a user name frappe-user
    - A user with admin permission is added
    - this is useful for security
    - switch to the user and change directory /home/frappe-user/

--- v14script.sh
+++ v14script.sh
@@ -8,3 +8,13 @@ sudo apt upgrade -y
 
 sudo apt install aptitude -y
 
+# We create this user as a security measure to prevent root user access.
+# you can change username here change the "frappe-user" to "Username"
+
+sudo adduser frappe-user
+
+# This user will be assigned admin permissions and will be used as the main Frappe Bench user
+
+sudo usermod -aG sudo frappe-user
+su frappe-user 
+cd /home/frappe-user/
